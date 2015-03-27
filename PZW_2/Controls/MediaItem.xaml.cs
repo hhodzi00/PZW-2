240ecfad30b576e06bd9790cec8cbd230b1135d6
hhodzi00 <hhodzi00@fesb.hr>
2015-03-27 18:55:52 +0100

    A

---
+++ PZW_2/Controls/MediaItem.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PZW_2.Controls
+{
+    /// <summary>
+    /// Interaction logic for MediaItem.xaml
+    /// </summary>
+    public partial class MediaItem : UserControl
+    {
+        public MediaItem()
+        {
+            InitializeComponent();
+        }
+
+        //propd pa tab tab
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(
+                "Title",
+                typeof(string), 
+                typeof(MediaItem),
+                new UIPropertyMetadata("Title")
+           );
+
+        public static readonly RoutedEvent DeleteEvent = EventManager.RegisterRoutedEvent
+        (
+           "Delete", //ime eventa
+            RoutingStrategy.Bubble,
+            typeof(RoutedEventHandler),
+            typeof(MediaItem) //tip elementa koji posjeduje event
+        );
+
+        public event RoutedEventHandler Delete //za registraciju/deregistraciju 
+        {
+            add { AddHandler(DeleteEvent, value); }
+            remove { RemoveHandler(DeleteEvent, value); }
+        }
+
+        void RaiseDeleteEvent()
+        {
+            RoutedEventArgs newEventArgs = new RoutedEventArgs(MediaItem.DeleteEvent);
+            RaiseEvent(newEventArgs);
+        }
+
+        private void OnControlLoaded(object sender, RoutedEventArgs e)
+        {
+            this.DeleteIcon.MouseDown += new MouseButtonEventHandler(DeleteIcon_MouseDown);
+        }        
+
+        void DeleteIcon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            RaiseDeleteEvent();
+        }
+
+    }
+}
---
+++ PZW_2/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PZW_2.Controls;
+namespace PZW_2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            this.AddButton.Click += AddButton_Click;
+            
+            this.LeftButton.Click += (objSender, eventArgs) => SelectPreviousMediaItem();
+            this.RightButton.Click += (objSender, eventArgs) => SelectNextMediaItem();
+
+            foreach (var child in this.MediaItemContainer.Children)
+            {
+                if (!(child is MediaItem)) { continue; }
+            
+                var mediaItem = child as MediaItem;//castanje kao i (MediaItem)child
+                mediaItem.Delete += OnMediaItem_Delete;
+                
+            }
+        }
+
+        void OnMediaItem_Delete(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is MediaItem)) { return; }
+
+            var mediaItem = sender as MediaItem;
+
+            this.MediaItemContainer.Children.Remove(mediaItem);
+            
+        }
+
+        private int _selectedIndex = 0;
+
+        private void SelectPreviousMediaItem()
+        {
+            if (_selectedIndex <= 0) { return;  }
+
+            _VisuallyDeselectSelectedMediaItem();
+
+            _selectedIndex--;
+
+            _VisuallySelectSelectedMediaItem();
+        }
+
+        private void SelectNextMediaItem()
+        {
+            if (_selectedIndex >= this.MediaItemContainer.Children.Count - 1) { return; }
+
+            _VisuallyDeselectSelectedMediaItem();
+
+            _selectedIndex++;
+
+            _VisuallySelectSelectedMediaItem();
+        }
+
+        private void _VisuallySelectSelectedMediaItem()
+        {
+            if (_IsNotValidIndex()) { return; }
+
+            var element = this.MediaItemContainer.Children[_selectedIndex];
+        }
+
+        private void _VisuallyDeselectSelectedMediaItem()
+        {
+            if (_IsNotValidIndex()) { return; }
+
+            var element = this.MediaItemContainer.Children[_selectedIndex];
+        }
+
+        private bool _IsNotValidIndex()
+        {
+            return _selectedIndex < 0 || _selectedIndex >= this.MediaItemContainer.Children.Count;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
